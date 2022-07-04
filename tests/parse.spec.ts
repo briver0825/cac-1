2ad4ab912a337231f303a2ec5d2a6b956e60fb55
cuixiaorui <cui_xiaorui@126.com>
2022-07-04 14:48:57 +0800

    test: extract parse unit test

---
+++ tests/parse.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from "vitest";
+import { cac } from "../src";
+
+test("hapy path", () => {
+  const cli = cac();
+
+  const rawArrs1 = ["--type", "foo", "bar"];
+  expect(cli.parse(rawArrs1)).toEqual({
+    args: ["bar"],
+    options: {
+      type: "foo",
+    },
+  });
+
+  const rawArrs2 = ["--type", "foo", "name", "--bar", "bar"];
+  expect(cli.parse(rawArrs2)).toEqual({
+    args: ["name"],
+    options: {
+      type: "foo",
+      bar: "bar",
+    },
+  });
+});
