16bc080c71418f603d1c9d0d22a2593e3a218596
cuixiaorui <cui_xiaorui@126.com>
2022-07-09 21:11:47 +0800

    style: fix parse.spec style

--- tests/parse.spec.ts
+++ tests/parse.spec.ts
@@ -1,23 +1,23 @@
-import { expect, test } from "vitest";
-import { cac } from "../src";
+import { expect, test } from 'vitest'
+import { cac } from '../src'
 
-test.skip("hapy path", () => {
-  const cli = cac();
+test.skip('hapy path', () => {
+  const cli = cac()
 
-  const rawArrs1 = ["--type", "foo", "bar"];
+  const rawArrs1 = ['--type', 'foo', 'bar']
   expect(cli.parse(rawArrs1)).toEqual({
-    args: ["bar"],
+    args: ['bar'],
     options: {
-      type: "foo",
-    },
-  });
+      type: 'foo'
+    }
+  })
 
-  const rawArrs2 = ["--type", "foo", "name", "--bar", "bar"];
+  const rawArrs2 = ['--type', 'foo', 'name', '--bar', 'bar']
   expect(cli.parse(rawArrs2)).toEqual({
-    args: ["name"],
+    args: ['name'],
     options: {
-      type: "foo",
-      bar: "bar",
-    },
-  });
-});
+      type: 'foo',
+      bar: 'bar'
+    }
+  })
+})
